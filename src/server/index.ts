f078c929bac0240fcc5a2df0ced3234ca1c520f1
Grant <65585002+ColonelParrot@users.noreply.github.com>
2026-06-22 23:19:07 -0700

    create beta cli

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,144 +1,54 @@
-﻿import express, { type Request, type Response } from "express";
-import { WebSocketServer } from "ws";
+#!/usr/bin/env node
 
-/**
- * inbound messages from WebSocket
- */
-interface SocketMessage {
-    success: boolean,
-    message: string | null,
-    id: string, // every message must be associated w/ request for multi-agent
-    data: string | null // usually base64
-}
+import { Command } from "commander";
 
-/**
- * identifies request to fulfill w/ ID
- */
-const requestMap: Map<string, Response> = new Map()
+const program = new Command()
+const baseUrl = 'http://localhost:7336'
 
-const wss = new WebSocketServer({ port: 7335 });
-console.log('WebSocket server started.')
+program.name('peeked')
+    .description('Connect visual agents with your browser.')
+    .version('0.0.1');
 
-const heartbeatInterval = setInterval(() => {
-    wss.clients.forEach(ws => {
-        if (ws.isAlive === false) {
-            return ws.terminate();
-        }
+program.argument('[url]', 'The page URL to capture screenshot of.')
+    .option('-l, --list', 'List all tracked URLs.')
+    .action(async (url, options) => {
+        if (options.list) {
+            try {
+                const response = await fetch(`${baseUrl}/urls`)
+                const { success, message, data } = await response.json()
+                if (success) {
+                    console.log(data)
+                } else {
+                    console.error('Failed. ' + message);
+                }
+            } catch (error) {
+                console.log('Server not started. Did you run: peeked begin?')
+            }
+        } else if (url) {
+            try {
 
-        ws.isAlive = false;
-        ws.ping();
-    })
-}, 20 * 1000)
+                const response = await fetch(`${baseUrl}/send`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ url: url }),
+                })
 
-wss.on('close', () => {
-    clearInterval(heartbeatInterval)
-})
-
-wss.on('connection', (ws) => {
-    ws.isAlive = true;
-
-    ws.on('pong', () => {
-        ws.isAlive = true;
-    })
-
-    ws.on('message', (raw) => {
-        const message: SocketMessage = JSON.parse(raw.toString())
-        const req = requestMap.get(message.id);
-
-        if (req) {
-            req.json({
-                success: message.success,
-                message: message.message,
-                data: message.data
-            })
-            requestMap.delete(message.id)
+                const { success, message, data } = await response.json()
+                if (success) {
+                    console.log(data)
+                } else {
+                    console.error('Failed. ' + message);
+                }
+            } catch (error) {
+                console.log('Server not started. Did you run: peeked begin?')
+            }
+        } else {
+            program.help()
         }
     })
-})
 
-// ======== EXPRESS ========
+program.parse(process.argv)
 
-/**
- * express server response
- */
-interface ServerResponse {
-    success: boolean;
-    message: string | null;
-    data: string | null;
-}
-
-interface OutboundSocketMessage {
-    id: string;
-    type: "screenshot" | "urls",
-    url?: string;
-}
-
-const app = express()
-app.use(express.json())
-
-app.get('/urls', (req: Request, res: Response) => {
-    if (wss.clients.size === 0) {
-        return res.json({
-            success: false,
-            message: "Chrome extension not connected.",
-            data: null
-        })
-    }
-
-    const ws = Array.from(wss.clients)[0];
-    const id = crypto.randomUUID()
-
-    requestMap.set(id, res);
-    ws.send(JSON.stringify({
-        id,
-        type: "urls"
-    }))
-
-    setTimeout(() => {
-        if (requestMap.has(id)) {
-            res.json({
-                success: false,
-                message: "Chrome extension timed out.",
-                data: null
-            })
-            requestMap.delete(id)
-        }
-    }, 10000)
-})
-
-app.post('/send', (req: Request, res: Response) => {
-    if (wss.clients.size === 0) {
-        return res.json({
-            success: false,
-            message: "Chrome extension not connected.",
-            data: null
-        })
-    }
-
-    const ws = Array.from(wss.clients)[0];
-    const id = crypto.randomUUID()
-
-    const { url } = req.body;
-
-    requestMap.set(id, res);
-    ws.send(JSON.stringify({
-        id,
-        type: "screenshot",
-        url
-    }))
-
-    setTimeout(() => {
-        if (requestMap.has(id)) {
-            res.json({
-                success: false,
-                message: "Chrome extension timed out.",
-                data: null
-            })
-            requestMap.delete(id)
-        }
-    }, 10000)
-});
-
-app.listen(7336, () => {
-    console.log("CLI server started.")
-})
+// ADD START COMMAND!
---
+++ src/server/server.ts
@@ -0,0 +1,144 @@
+﻿import express, { type Request, type Response } from "express";
+import { WebSocketServer } from "ws";
+
+/**
+ * inbound messages from WebSocket
+ */
+interface SocketMessage {
+    success: boolean,
+    message: string | null,
+    id: string, // every message must be associated w/ request for multi-agent
+    data: string | null // usually base64
+}
+
+/**
+ * identifies request to fulfill w/ ID
+ */
+const requestMap: Map<string, Response> = new Map()
+
+const wss = new WebSocketServer({ port: 7335 });
+console.log('WebSocket server started.')
+
+const heartbeatInterval = setInterval(() => {
+    wss.clients.forEach(ws => {
+        if (ws.isAlive === false) {
+            return ws.terminate();
+        }
+
+        ws.isAlive = false;
+        ws.ping();
+    })
+}, 20 * 1000)
+
+wss.on('close', () => {
+    clearInterval(heartbeatInterval)
+})
+
+wss.on('connection', (ws) => {
+    ws.isAlive = true;
+
+    ws.on('pong', () => {
+        ws.isAlive = true;
+    })
+
+    ws.on('message', (raw) => {
+        const message: SocketMessage = JSON.parse(raw.toString())
+        const req = requestMap.get(message.id);
+
+        if (req) {
+            req.json({
+                success: message.success,
+                message: message.message,
+                data: message.data
+            })
+            requestMap.delete(message.id)
+        }
+    })
+})
+
+// ======== EXPRESS ========
+
+/**
+ * express server response
+ */
+interface ServerResponse {
+    success: boolean;
+    message: string | null;
+    data: string | null;
+}
+
+interface OutboundSocketMessage {
+    id: string;
+    type: "screenshot" | "urls",
+    url?: string;
+}
+
+const app = express()
+app.use(express.json())
+
+app.get('/urls', (req: Request, res: Response) => {
+    if (wss.clients.size === 0) {
+        return res.json({
+            success: false,
+            message: "Chrome extension not connected.",
+            data: null
+        })
+    }
+
+    const ws = Array.from(wss.clients)[0];
+    const id = crypto.randomUUID()
+
+    requestMap.set(id, res);
+    ws.send(JSON.stringify({
+        id,
+        type: "urls"
+    }))
+
+    setTimeout(() => {
+        if (requestMap.has(id)) {
+            res.json({
+                success: false,
+                message: "Chrome extension timed out.",
+                data: null
+            })
+            requestMap.delete(id)
+        }
+    }, 10000)
+})
+
+app.post('/send', (req: Request, res: Response) => {
+    if (wss.clients.size === 0) {
+        return res.json({
+            success: false,
+            message: "Chrome extension not connected.",
+            data: null
+        })
+    }
+
+    const ws = Array.from(wss.clients)[0];
+    const id = crypto.randomUUID()
+
+    const { url } = req.body;
+
+    requestMap.set(id, res);
+    ws.send(JSON.stringify({
+        id,
+        type: "screenshot",
+        url
+    }))
+
+    setTimeout(() => {
+        if (requestMap.has(id)) {
+            res.json({
+                success: false,
+                message: "Chrome extension timed out.",
+                data: null
+            })
+            requestMap.delete(id)
+        }
+    }, 10000)
+});
+
+app.listen(7336, () => {
+    console.log("CLI server started.")
+})
