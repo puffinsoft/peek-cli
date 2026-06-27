4355171d951490c2f726dec8e63eca25427393e0
Grant <65585002+ColonelParrot@users.noreply.github.com>
2026-06-27 15:07:47 -0700

    remove storage

--- src/extension/utils/constants.ts
+++ src/extension/utils/constants.ts
@@ -4,12 +4,7 @@
 export const messageKeys = {
     showGlow: "show",
     hideGlow: "hide",
-    setStorage: "setStorage", // deprecate soon
     connect: "connect",
     disconnect: "disconnect",
     status: "status"
-}
-
-export const storageKeys = {
-    openIfMissing: "open_if_missing"
 }
