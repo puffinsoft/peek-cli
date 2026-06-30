7455392d3df4c851038d01b433e612b82c49df07
Grant <65585002+ColonelParrot@users.noreply.github.com>
2026-06-29 23:10:09 -0700

    add queueing (#2)

--- src/extension/background.ts
+++ src/extension/background.ts
@@ -10,13 +10,99 @@ let activeWs = null;
 chrome.runtime.onStartup.addListener(() => updateIconByWs(activeWs))
 chrome.tabs.onActivated.addListener(() => updateIconByWs(activeWs))
 
+type CaptureRequest = {
+    id: string;
+    url: string;
+}
 
+let urlQueue: Array<CaptureRequest> = []
+let currentlyScanning = false;
+
+
+const scan = async () => {
+    if (currentlyScanning) return;
+    if (urlQueue.length == 0) return;
+
+    currentlyScanning = true;
+    try {
+        while (urlQueue.length > 0) {
+            const { id, url } = urlQueue.shift();
+            try {
+                const targetTab = await findTab(url);
+                if (!targetTab) {
+                    activeWs.send(JSON.stringify({
+                        success: false,
+                        id,
+                        message: "Failed to find tab with URL " + url,
+                        data: null
+                    }))
+                } else {
+                    const { tab, prevTab } = targetTab;
+                    await chrome.windows.update(tab.windowId, { focused: true });
+                    await chrome.tabs.update(tab.id, { active: true });
+                    // Small delay to ensure the OS / Chrome has rendered the switch visually
+                    await new Promise((resolve) => setTimeout(resolve, 150));
+
+
+                    const screenshotDataURL = await chrome.tabs.captureVisibleTab()
+
+                    /**
+                     * only glow for the last screenshot to speed up capture flow.
+                     * must extract to var here in case new capture request comes in
+                     * during capture
+                     */
+                    const shouldGlow = urlQueue.length === 0;
+
+                    if (shouldGlow) {
+                        try {
+                            await chrome.tabs.sendMessage(tab.id, { type: messageKeys.showGlow })
+                        } catch (error) {
+                            // content script not injected
+                        }
+                    }
+
+                    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+                    if (shouldGlow) {
+                        try {
+                            await chrome.tabs.sendMessage(tab.id, { type: messageKeys.hideGlow })
+                        } catch (error) { }
+                    }
+
+                    activeWs.send(JSON.stringify({
+                        success: true,
+                        id,
+                        message: "Successfully glowed URL " + url,
+                        data: screenshotDataURL
+                    }))
+
+                    await new Promise((resolve) => setTimeout(resolve, 600));
+                    if (prevTab && tab.id !== prevTab.id) {
+                        await chrome.windows.update(prevTab.windowId, { focused: true });
+                        await chrome.tabs.update(prevTab.id, { active: true });
+                    }
+                }
+            } catch (error) {
+                activeWs.send(JSON.stringify({
+                    success: false,
+                    id,
+                    message: "An unexpected error occured. Reason: " + error.name,
+                    data: null
+                }))
+            }
+        }
+    } finally {
+        currentlyScanning = false;
+    }
+}
 
 /**
  * attempts to find the matching tab for requested URL.
  * checks if base URL matches requested URL.
  * strips out params & hash.
  * prioritizes active tab first.
+ * 
+ * WARNING: prevTab can be null.
  * 
  * @returns id of matching tab, null if not found.
  * ^ null handling (opening if not found) will be added later.
@@ -39,7 +125,7 @@ const findTab = async (url: string) => {
         if (urlMatches(tab.url, url)) {
             return {
                 tab,
-                prevTab: topTabs[0]
+                prevTab: topTabs.length > 0 ? topTabs[0] : null
             }
         }
     }
@@ -73,50 +159,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             const { id, type, url } = JSON.parse(event.data);
 
             if (type === "screenshot") {
-                const targetTab = await findTab(url);
-                if (targetTab === null) {
-                    activeWs.send(JSON.stringify({
-                        success: false,
-                        id,
-                        message: "Failed to find tab with URL " + url,
-                        data: null
-                    }))
-                } else {
-                    const { tab, prevTab } = targetTab;
-                    await chrome.windows.update(tab.windowId, { focused: true });
-                    await chrome.tabs.update(tab.id, { active: true });
-                    // Small delay to ensure the OS / Chrome has rendered the switch visually
-                    await new Promise((resolve) => setTimeout(resolve, 150));
+                urlQueue.push({
+                    id,
+                    url
+                })
 
-
-                    const screenshotDataURL = await chrome.tabs.captureVisibleTab()
-
-                    try {
-                        await chrome.tabs.sendMessage(tab.id, { type: messageKeys.showGlow })
-                    } catch (error) {
-                        // content script not injected
-                    }
-                    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-                    try {
-                        await chrome.tabs.sendMessage(tab.id, { type: messageKeys.hideGlow })
-                    } catch (error) { }
-
-
-                    activeWs.send(JSON.stringify({
-                        success: true,
-                        id,
-                        message: "Successfully glowed URL " + url,
-                        data: screenshotDataURL
-                    }))
-
-                    await new Promise((resolve) => setTimeout(resolve, 600));
-                    if (tab.id !== prevTab.id) {
-                        await chrome.windows.update(prevTab.windowId, { focused: true });
-                        await chrome.tabs.update(prevTab.id, { active: true });
-                    }
-                }
-            } else if(type === "urls"){
+                if (!currentlyScanning) scan()
+            } else if (type === "urls") {
                 const tabs = await chrome.tabs.query({});
 
                 activeWs.send(JSON.stringify({
